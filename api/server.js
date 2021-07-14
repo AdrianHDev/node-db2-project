c0a87519c6feb6960f8a964b5fe478195b1161e0
AdrianHDev <adrianhensondev@gmail.com>
2021-07-13 20:26:28 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const carsRouter = require("./cars/cars-router");
 
 const server = express();
 
 // DO YOUR MAGIC
 server.use(express.json());
+// Enable Cars Router.
+server.use('/api/cars', carsRouter);
 
 server.use("*", (_req, _res, next) => {
     next({ status: 404, message: "not found"})
